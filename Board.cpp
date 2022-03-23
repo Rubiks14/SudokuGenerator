b277819c437e6cdf8231a9f91f31e429d9d44758
Derek Thompson <derekallenthompson@gmail.com>
2022-03-23 12:00:31 -0500

    Update Board.cpp
    Added some error handling to Board class

--- Board.cpp
+++ Board.cpp
@@ -3,7 +3,7 @@
 #include <vector>
 #include "Board.h"
 
-Board::Board() : m_cells{} 
+Board::Board() : m_cells{} noexcept
 {
 	m_cells.fill(0);
 }
@@ -17,11 +17,16 @@ const unsigned short Board::operator[](std::size_t i) const
 
 const unsigned short Board::getCell(const unsigned short row, const unsigned short col) const
 {
+	if (row < 0 || row >= HEIGHT || col < 0 || col >= WIDTH)
+		throw std::range_error("row and col must be between [0-" + WIDTH + ')');
 	return m_cells[row * WIDTH + col];
 }
 
 std::vector<unsigned short> Board::getAvailability(const unsigned short row, const unsigned short col) const
 {
+	if (row < 0 || row >= HEIGHT || col < 0 || col >= WIDTH)
+		throw std::range_error("row and col must be between [0-" + WIDTH + ')');
+	
 	std::vector<unsigned short> available{ 1, 2, 3, 4, 5, 6, 7, 8, 9 };
 
 	const unsigned short BlockStartRow = row / 3 * 3;
@@ -51,17 +56,25 @@ std::vector<unsigned short> Board::getAvailability(const unsigned short row, con
 
 void Board::fillCell(const unsigned short row, const unsigned short col, const unsigned short value)
 {
+	if (row < 0 || row >= HEIGHT || col < 0 || col >= WIDTH)
+		throw std::range_error("row and col must be between [0-" + WIDTH + ')');
+	if (value < 1 || value > 9)
+		throw std::out_of_range("Values can only be between 1 and 9");
 	m_cells[row * WIDTH + col] = value;
 }
 
 void Board::emptyCell(const unsigned short row, const unsigned short col)
 {
+	if (row < 0 || row >= HEIGHT || col < 0 || col >= WIDTH)
+		throw std::range_error("row and col must be between [0-" + WIDTH + ')');
 	if (m_cells[row * WIDTH + col] != 0)
 		m_cells[row * WIDTH + col] = 0;
 }
 
 void Board::clearRow(const unsigned short row)
 {
+	if (row < 0 || row >= HEIGHT)
+		throw std::range_error("row must be between [0-" + WIDTH + ')');
 	for (unsigned short i = 0; i < WIDTH; ++i)
 	{
 		emptyCell(row, i);
